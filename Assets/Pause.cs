54017b68d26ab040a80e8a69ad9400d2bf167b22
Evan Rosson <github@erosson.org>
2014-03-14 22:57:20 -0700

    crude pause; gitignore

---
+++ Assets/Pause.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	public GameObject paused;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (paused.activeSelf) {
+				// unpause
+				Time.timeScale = 1;
+			}
+			else {
+				// pause
+				Time.timeScale = 0;
+			}
+			paused.SetActive (!paused.activeSelf);
+		}
+	}
+}
