45d95b66424f9324db7b639abc158bc8907e4e75
Evan Rosson <github@erosson.org>
2014-03-16 02:11:52 -0700

    Visible timer, does nothing on expire. #14

---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	public delegate void TimeOverEvent();
+	public event TimeOverEvent TimeOver = delegate {};
+
+	public int timelimitSeconds = 180;
+
+	void Start() {
+		TimeOver += () => Debug.Log ("timeover!");
+
+		StartCoroutine(Countdown(new System.TimeSpan(0, 0, timelimitSeconds)));
+	}
+
+	private IEnumerator Countdown(System.TimeSpan remaining) {
+		// Instead of using a starttime, this accounts for pauses/timescale
+		Display(remaining);
+		var second = new System.TimeSpan(0, 0, 1);
+		while (remaining.TotalSeconds > 0) {
+			yield return new WaitForSeconds(1);
+			remaining = remaining.Subtract(second);
+			Display(remaining);
+		}
+		TimeOver();
+	}
+
+	private void Display(System.TimeSpan remaining) {
+		guiText.text = string.Format("{0:0}:{1:00}", remaining.Minutes, remaining.Seconds, remaining.Milliseconds);
+	}
+}
