685f62a205ab578f66a87e621fb54c847653bc69
Evan Rosson <github@erosson.org>
2014-03-15 19:40:27 -0700

    Rework music as singleton. Main menu sfx. #16 #11

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
+	public AudioClip select;
+	public AudioClip cancel;
+
 	void OnGUI() {
 		GUILayout.BeginArea(new Rect(Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/2));
 		if (GUILayout.Button("Play")) {
@@ -20,10 +23,13 @@ public class MainMenu : MonoBehaviour {
 	}
 
 	private void Play () {
+		// Singletons are icky, but needed to play the sound properly after the menu's destroyed by loadlevel
+		Music.Instance.audio.PlayOneShot(select);
 		Application.LoadLevel("Game");
 	}
 
 	private void Exit() {
+		Music.Instance.audio.PlayOneShot(cancel);
 		Application.Quit();
 	}
 }
--- Assets/Music.cs
+++ Assets/Music.cs
@@ -2,7 +2,23 @@
 using System.Collections;
 
 public class Music : MonoBehaviour {
+	// http://answers.unity3d.com/questions/11314/audio-or-music-to-continue-playing-between-scene-c.html
+	private static Music _singleton = null;
+	public static Music Instance {
+		get {
+			return _singleton;
+		}
+	}
+
 	void Start () {
-		DontDestroyOnLoad(gameObject);
+		if (_singleton != null) {
+			if (_singleton != this) {
+				Destroy (this.gameObject);
+			}
+		}
+		else {
+			_singleton = this;
+			DontDestroyOnLoad(gameObject);
+		}
 	}
 }
