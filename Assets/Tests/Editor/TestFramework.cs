b763a0f8f9f5ff4914b7a9000640302c30b5a091
Evan Rosson <github@erosson.org>
2014-03-15 22:57:56 -0700

    First unit test! #20

---
+++ Assets/Tests/Editor/TestFramework.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+// Make sure the test framework's working.
+internal class TestFramework {
+	[Test]
+	public void testPass() {
+		Assert.Pass();
+	}
+
+	[Test]
+	[Ignore]
+	public void testIgnore() {
+		Assert.Fail();
+	}
+}
