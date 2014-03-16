ab7570772c91921f84e9cb8c98d651b893b841ce
Evan Rosson <github@erosson.org>
2014-03-15 18:03:43 -0700

    Mainmenu. #6, fixes #7

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	void OnGUI() {
+		Debug.Log ("guilol");
+		GUILayout.BeginArea(new Rect(Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/2));
+		if (GUILayout.Button("Play")) {
+			Play();
+		}
+		if (GUILayout.Button("Exit")) {
+			Exit();
+		}
+		GUILayout.EndArea();
+	}
+
+	private void Play () {
+		Application.LoadLevel("Game");
+	}
+
+	private void Exit() {
+		Application.Quit();
+	}
+}
